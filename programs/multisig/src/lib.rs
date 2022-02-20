b2f88c5366b0c0a66057430a911ead4b580d7baf
Tengiz Sharafiev <btolfa@gmail.com>
2022-02-20 15:23:26 +0700

    Add create_multisig instruction and tests

--- programs/multisig/src/lib.rs
+++ programs/multisig/src/lib.rs
@@ -5,10 +5,81 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod multisig {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    
+    // Initializes a new multisig account with a set of owners and a threshold.
+    pub fn create_multisig(
+        ctx: Context<CreateMultisig>,
+        owners: Vec<Pubkey>,
+        threshold: u64,
+    ) -> ProgramResult {
+        require!(!owners.is_empty() && owners.len() <= OWNERS_MAX_SIZE, InvalidOwnersLen);
+        require!(threshold > 0 && threshold < owners.len() as u64, InvalidThreshold);
+        assert_unique_owners(&owners)?;
+
+        let multisig = &mut ctx.accounts.multisig;
+        multisig.owners = owners;
+        multisig.threshold = threshold;
+        multisig.nonce = *ctx.bumps.get("multisig_signer").unwrap();
+        multisig.owner_set_seqno = 0;
         Ok(())
     }
 }
 
+fn assert_unique_owners(owners: &[Pubkey]) -> ProgramResult {
+    for (i, owner) in owners.iter().enumerate() {
+        require!(
+            !owners.iter().skip(i + 1).any(|item| item == owner),
+            UniqueOwners
+        )
+    }
+    Ok(())
+}
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateMultisig<'info> {
+    #[account(init, payer = payer, space = MULTISIG_SIZE)]
+    pub multisig: Account<'info, Multisig>,
+    #[account(seeds = [multisig.key().as_ref()], bump)]
+    pub multisig_signer: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+const OWNERS_MAX_SIZE : usize = 8;
+const MULTISIG_SIZE : usize = 8 + // discriminator
+    std::mem::size_of::<u64>() + // threshold
+    std::mem::size_of::<u8>() + // nonce
+    std::mem::size_of::<u32>() + // owner_set_seqno
+    4 + std::mem::size_of::<Pubkey>()*OWNERS_MAX_SIZE; // owners
+
+#[account]
+pub struct Multisig {
+    pub owners: Vec<Pubkey>,
+    pub threshold: u64,
+    pub nonce: u8,
+    pub owner_set_seqno: u32,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("The given owner is not part of this multisig.")]
+    InvalidOwner,
+    #[msg("Owners length must be non zero and less than OWNERS_MAX_SIZE")]
+    InvalidOwnersLen,
+    #[msg("Not enough owners signed this transaction.")]
+    NotEnoughSigners,
+    #[msg("Cannot delete a transaction that has been signed by an owner.")]
+    TransactionAlreadySigned,
+    #[msg("Overflow when adding.")]
+    Overflow,
+    #[msg("Cannot delete a transaction the owner did not create.")]
+    UnableToDelete,
+    #[msg("The given transaction has already been executed.")]
+    AlreadyExecuted,
+    #[msg("Threshold must be less than or equal to the number of owners.")]
+    InvalidThreshold,
+    #[msg("Owners must be unique")]
+    UniqueOwners,
+}
--- tests/multisig.ts
+++ tests/multisig.ts
@@ -1,17 +1,106 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Multisig } from '../target/types/multisig';
+import { expect } from 'chai'
+
+import * as chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 
 describe('multisig', () => {
-
+  
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Multisig as Program<Multisig>;
+  const provider = anchor.getProvider();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const multisig = anchor.web3.Keypair.generate();
+
+  const ownerA = anchor.web3.Keypair.generate();
+  const ownerB = anchor.web3.Keypair.generate();
+  const ownerC = anchor.web3.Keypair.generate();
+  const ownerD = anchor.web3.Keypair.generate();
+  const owners = [ownerA.publicKey, ownerB.publicKey, ownerC.publicKey];
+
+  it('Should create multisig 2 out of 3', async () => {
+    await program.methods
+      .createMultisig(owners, new anchor.BN(2))
+      .accounts({multisig: multisig.publicKey})
+      .signers([multisig])
+      .rpc();
+
+    const multisigAccount = await program.account.multisig.fetch(multisig.publicKey);
+    expect(multisigAccount.threshold.toNumber()).to.be.equal(2);
+    expect(multisigAccount.owners).to.be.deep.equal(owners);
+  });
+
+  it('Should NOT create multisig with nonuniq owners', async () => {
+    const ms = anchor.web3.Keypair.generate();
+    const nonuniqowners = [ownerA.publicKey, ownerB.publicKey, ownerB.publicKey];
+    await expect(
+      program.methods
+        .createMultisig(nonuniqowners, new anchor.BN(2))
+        .accounts({multisig: ms.publicKey})
+        .signers([ms])
+        .rpc())
+      .to.be.rejectedWith(/Owners must be unique/);
+  });
+
+  it('Should NOT create multisig zero owners', async () => {
+    const ms = anchor.web3.Keypair.generate();
+    await expect(
+      program.methods
+        .createMultisig([], new anchor.BN(2))
+        .accounts({multisig: ms.publicKey})
+        .signers([ms])
+        .rpc())
+      .to.be.rejectedWith(/Owners length must be non zero and less than OWNERS_MAX_SIZE/);
+
+  });
+
+  it('Should NOT create multisig too many owners', async () => {
+    const ms = anchor.web3.Keypair.generate();
+
+    const owner4 = anchor.web3.Keypair.generate();
+    const owner5 = anchor.web3.Keypair.generate();
+    const owner6 = anchor.web3.Keypair.generate();
+    const owner7 = anchor.web3.Keypair.generate();
+    const owner8 = anchor.web3.Keypair.generate();
+    const owner9 = anchor.web3.Keypair.generate();
+    const owners9 = [ownerA.publicKey, ownerB.publicKey, ownerB.publicKey, owner4.publicKey, owner5.publicKey, owner6.publicKey,
+       owner7.publicKey, owner8.publicKey, owner9.publicKey];
+
+    await expect(
+      program.methods
+        .createMultisig(owners9, new anchor.BN(2))
+        .accounts({multisig: ms.publicKey})
+        .signers([ms])
+        .rpc())
+      .to.be.rejectedWith(/Owners length must be non zero and less than OWNERS_MAX_SIZE/);
+
+  });
+
+  it('Should NOT create multisig with zero threshold', async () => {
+    const ms = anchor.web3.Keypair.generate();
+    await expect(
+      program.methods
+        .createMultisig(owners, new anchor.BN(0))
+        .accounts({multisig: ms.publicKey})
+        .signers([ms])
+        .rpc())
+      .to.be.rejectedWith(/Threshold must be less than or equal to the number of owners/);
+  });
+
+  it('Should NOT create multisig with incorrect threshold', async () => {
+    const ms = anchor.web3.Keypair.generate();
+    await expect(
+      program.methods
+        .createMultisig(owners, new anchor.BN(4))
+        .accounts({multisig: ms.publicKey})
+        .signers([ms])
+        .rpc())
+      .to.be.rejectedWith(/Threshold must be less than or equal to the number of owners/);
+
   });
 });
